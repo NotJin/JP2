f1f9827cdf655db462394a3b461bf486e9fe737b
Đồng Vũ <Dongvu7173@gmail.com>
2022-11-08 10:25:04 +0700

    JP2

---
+++ src/demo_collection/Car.java
@@ -0,0 +1,31 @@
+package demo_collection;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Car {
+    private String brand;
+    private Integer produceYear;
+
+    public Car(String brand, Integer produceYear) {
+        this.brand = brand;
+        this.produceYear = produceYear;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getProduceYear() {
+        return produceYear;
+    }
+
+    public void setProduceYear(Integer produceYear) {
+        this.produceYear = produceYear;
+    }
+
+}
